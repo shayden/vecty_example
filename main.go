4aee7083ff931c36815556104466d91c89aca5f7
Sean <ctordtor@outlook.com>
2020-07-25 14:58:49 -0400

    Simple button to change state.

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gopherjs/vecty"
 	"github.com/gopherjs/vecty/elem"
+	"github.com/gopherjs/vecty/event"
+	"github.com/gopherjs/vecty/prop"
 )
 
 func main() {
@@ -12,10 +16,36 @@ func main() {
 
 type PageView struct {
 	vecty.Core
+
+	Clicked bool
+}
+
+func (p *PageView) onClicked(event *vecty.Event) {
+	if p.Clicked {
+		println("clicked")
+	} else {
+		println("not clicked")
+	}
+	p.Clicked = !p.Clicked
+
+	// Unlike react, you get to figure out when to re-render.
+	vecty.Rerender(p)
 }
 
 func (p *PageView) Render() vecty.ComponentOrHTML {
 	return elem.Body(
-		vecty.Text("Hello Vecty! Whee"),
+		elem.Div(vecty.Text("Hi!")),
+		elem.Div(vecty.Text("Hello Vecty! Whee")),
+		elem.Div(vecty.Text(strconv.FormatBool(p.Clicked))),
+		elem.Div(
+			elem.Input(
+				vecty.Markup(
+					prop.Type(prop.TypeButton),
+					event.Click(
+						p.onClicked,
+					),
+				),
+			),
+		),
 	)
 }
